bfc3ae5cd69b787074698ebf345f7bc652e2bd3c
sara-jaffar <sara.alshoula55@email.com>
2025-06-29 10:45:02 +0300

    Cache buttons and create state variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('does it work?')
+/*-------------------------------- Constants --------------------------------*/
+const choices = ['rock', 'paper', 'scissors'];
+
+
+/*-------------------------------- Variables --------------------------------*/
+let msg;
+let playerChoice;
+let computerChoice;
+
+
+/*------------------------ Cached Element References ------------------------*/
+const rockBtnEl = document.querySelector("#rock");
+const paperBtnEl = document.querySelector("paper");
+const scissorsBtnEl = document.querySelector("#scissors");
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
